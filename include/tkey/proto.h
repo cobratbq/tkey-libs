1ac9d65ad3d925df357f5cccce357e667e391dde
Danny van Heumen <danny@dannyvanheumen.nl>
2024-02-27 17:26:14 +0100

    proto: remove const where arguments would be copied

--- include/tkey/proto.h
+++ include/tkey/proto.h
@@ -34,7 +34,7 @@ struct frame_header {
 	size_t len;
 };
 
-uint8_t genhdr(const uint8_t id, const enum endpoints endpoint, const uint8_t status, const enum cmdlen len);
+uint8_t genhdr(uint8_t id, enum endpoints endpoint, uint8_t status, enum cmdlen len);
 int parseframe(uint8_t b, struct frame_header *hdr);
 void writebyte(uint8_t b);
 void write(const uint8_t *buf, size_t nbytes);
